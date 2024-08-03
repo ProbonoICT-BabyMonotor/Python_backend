0aa443c7d28b29f92ac1161328ad58f65b4d6690
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-08-04 02:51:51 +0900

    ✨feat: edit HTTP POST Method

--- function/http_request_spring.py
+++ function/http_request_spring.py
@@ -10,8 +10,9 @@ def getRequest(goto):
     return response.status_code
 
 # POST 요청 보내기 [추후 수정 필요]
-def postRequest(goto):
-    response = requests.post(ip+goto)
-    response = requests.post('https://jsonplaceholder.typicode.com/posts', json=payload)
-    print(response.status_code)  # 상태 코드 출력
-    print(response.json())       # JSON 응답 출력
+def postRequest(goto, payload):
+    response = requests.post(ip+goto, json=payload)
+    
+    # 상태 코드 확인
+    if response.status_code != 201:
+        print(response.json())       # JSON 응답 출력
